71f5f974dae471b4c38f10ba8b1c1295d846c458
Nitin <nitinchauhan.bsiotr@gmail.com>
2021-04-04 19:49:11 +0530

    newBook() & add() created in views.py AND new_book.html newBook form is created

--- BRM_app/models.py
+++ BRM_app/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title=models.CharField(max_length=100)
+    Title=models.CharField(max_length=100)
     Price=models.FloatField()
     Author=models.CharField(max_length=100)
     Publisher=models.CharField(max_length=100)
-    
--- BRM_app/views.py
+++ BRM_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from BRM_app.forms import NewBookForm
+from BRM_app.models import Book
 
 # Create your views here.
+def newBook(request):
+    form=NewBookForm()
+    res=render(request,'BRM_app/new_book.html',{'form':form})
+    return res
+def add(request):
+    if request.method=='POST':
+        form=NewBookForm(request.POST)
+        book=Book()
+        book.Title=form.data['title']
+        book.Price=form.data['price']
+        book.Author=form.data['author']
+        book.Publisher=form.data['publisher']
+        book.save()
+        s="Record Stored <br> <a href='/BRM_app/view-books'>View All Books</a>"
+        return HttpResponse(s)
