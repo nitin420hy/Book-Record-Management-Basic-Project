e00c160392c197dec1f8a9f2f1c1c593bf6e289d
Nitin <nitinchauhan.bsiotr@gmail.com>
2021-04-08 16:26:05 +0530

    Created Edit Book Record html page and view.py functions

--- BRM_app/views.py
+++ BRM_app/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from BRM_app.forms import NewBookForm
-from BRM_app.models import Book
+from BRM_app import models
 
 # Create your views here.
+def editBook(request):
+    book=models.Book.object.get(id=request.GET['bookid'])
+    fields={'title':book.Title,'price':book.Price,'author':book.Author,'Publisher':book.publisher}
+    form=NewBookForm(initial=fields)
+    res=render(request,'BRM_app/edit_book.html',{'form':form,'book',book})
+    return res
+
+def edit(request):
+    if request.method='POST':
+        form=NewBookForm(request.POST)
+        book=models.Book()
+        book.id=request.POST['bookid']
+        book.Title=form.data['title']
+        book.Price=form.data['price']
+        book.Author=form.data['author']
+        book.publisher=form.data['publisher']
+        book.save()
+        return HttpResponseRedirect('BRM_app/view_book.html')
+
 def viewBooks(request):
-    books=book.objects.all()
+    books=models.Book.objects.all()
     res=render(request,'BRM_app/view_book.html',{'books':books})
     return res
 
@@ -13,11 +32,11 @@ def newBook(request):
     form=NewBookForm()
     res=render(request,'BRM_app/new_book.html',{'form':form})
     return res
-    
+
 def add(request):
     if request.method=='POST':
         form=NewBookForm(request.POST)
-        book=Book()
+        book=models.Book()
         book.Title=form.data['title']
         book.Price=form.data['price']
         book.Author=form.data['author']
